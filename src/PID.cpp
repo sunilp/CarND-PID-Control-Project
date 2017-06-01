c2b03a793465f1ff27f402bb735e40f066131fd2
Sunil Prakash <prakashsunil@gmail.com>
2017-06-01 23:08:08 +0800

    updated

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,5 +1,6 @@
 #include "PID.h"
-
+#include <numeric>
+#include <math.h>
 using namespace std;
 
 /*
@@ -11,11 +12,38 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+
+  this->Kp = Kp; // P item
+  this->Ki = Ki; // I item
+  this->Kd = Kd; // D item
+
 }
 
 void PID::UpdateError(double cte) {
+
+  p_error = cte;
+  d_error = cte - p_error;
+  i_error += cte;
+
 }
 
 double PID::TotalError() {
+
+
+
+    double steer = -Kp * p_error -Kd * d_error - Ki * i_error;
+
+
+
+      if (fabs(steer)>1.0){
+        if (steer> 0){
+          steer -= 0.25;
+        } else {
+          steer += 0.25;
+        }
+      }
+
+	return steer;
+
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include "PID.h"
 #include <math.h>
 
+using namespace std;
 // for convenience
 using json = nlohmann::json;
 
@@ -33,7 +34,8 @@ int main()
   uWS::Hub h;
 
   PID pid;
-  // TODO: Initialize the pid variable.
+  //manually choosing the parameters after tuning
+  pid.Init(0.03,0.0003,1);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -57,6 +59,8 @@ int main()
           * NOTE: Feel free to play around with the throttle and speed. Maybe use
           * another PID controller to control the speed!
           */
+          pid.UpdateError(cte);
+          steer_value = pid.TotalError();
           
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
